e796cfa4dc8550cb71e24d887b4a07b90758a96c
Poonnakarn Panjasriprakarn <poon@Poonnakarns-MacBook-Pro.local>
2019-11-30 19:10:51 +0700

    es6-arrow-function tutorial

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -1,8 +1,6 @@
-"use strict";
-
 // argument objects - no longer bound
 
-var add = function add(a, b) {
+const add = (a, b) => {
   // console.log(argument)
   return a + b;
 };
@@ -11,12 +9,10 @@ console.log(add(10, 5));
 
 // This keyword bind to scope if ES5 function
 // แต่ this keybord is no longer bound in ES6 arrow function แต่จะตาม context
-var user = {
+const user = {
   name: "Poon",
   cities: ["Bangkok", "New York", "Cholburi"],
-  printPlacesLived: function printPlacesLived() {
-    var _this = this;
-
+  printPlacesLived() {
     // Foreach เข้าไปใน array แล้วทำทีละครั้ง
     // this.cities.forEach(city => {
     //   console.log(this.name + " has lived in " + city);
@@ -24,27 +20,21 @@ var user = {
 
     // Map return array ออกมา
     // ใช้ transform item ได้
-    return this.cities.map(function (city) {
-      return _this.name + " has lived in " + city + "!";
-    });
+    return this.cities.map(city => this.name + " has lived in " + city + "!");
   }
 };
 
 console.log(user.printPlacesLived());
 
 // Challenge
-var multiplier = {
+const multiplier = {
   // numbers - array of numbers
   // multiplyBy - single number
   // multiply - method return new array
   numbers: [10, 20, 30],
   multiplyBy: 3,
-  multiply: function multiply() {
-    var _this2 = this;
-
-    return this.numbers.map(function (number) {
-      return number * _this2.multiplyBy;
-    });
+  multiply() {
+    return this.numbers.map(number => number * this.multiplyBy);
   }
 };
 
---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,21 @@
+// const square = function(x) {
+//   return x * x;
+// };
+
+// //ES6 arrow function
+// // const squareArrow = x => {
+// //   return x * x;
+// // };
+
+// // shorthand arrow function expression syntax returns only 1 statement
+// const squareArrow = x => x * x;
+
+// console.log(squareArrow(4));
+
+// const getFirstName = fullName => {
+//   return fullName.split(" ")[0];
+// };
+
+const getFirstName = fullName => fullName.split(" ")[0];
+
+console.log(getFirstName("Poonnakarn Panjasriprakarn"));
